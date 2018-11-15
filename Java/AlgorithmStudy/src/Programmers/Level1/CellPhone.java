43bdd15b1f1bfda5f3719b295800719bc6615f86
KyeongMin <ekmst1027@gmail.com>
2018-11-16 00:06:02 +0900

    Hide Cellphone Number

--- Java/AlgorithmStudy/src/Programmers/Level1/CellPhone.java
+++ Java/AlgorithmStudy/src/Programmers/Level1/CellPhone.java
@@ -25,14 +25,18 @@
 package Programmers.Level1;
 
 public class CellPhone {
-	public String solution(String phone_number) {
+	public static String solution(String phone_number) {
 		String answer = "";
+		answer += phone_number.substring(phone_number.length()-4, phone_number.length());
+		for(int i = 0; i < phone_number.length()-4; i++) {
+			answer = "*" + answer;
+		}
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(solution("01033334444"));
+		System.out.println(solution("027778888"));
 	}
 
 }
